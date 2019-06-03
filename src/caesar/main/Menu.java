7bf4acb4b3e326766843a622e61a894ebe19f919
dominik <dominwro@gmail.com>
2019-06-04 00:02:31 +0200

    Adding saveToFile methods.

--- src/caesar/main/Menu.java
+++ src/caesar/main/Menu.java
@@ -1,34 +1,48 @@
 package caesar.main;
 
+import file.SaveToFile;
+
 import java.util.Scanner;
 
 public class Menu
 {
-    public static void main(String[] args)
+    public static void start()
     {
         Scanner scanner = new Scanner(System.in);
 
 
         Coding coding = new Coding();
-
-
-
-        System.out.println("1. Coding text, 2. Decodint text ");
-        int menu = scanner.nextInt();
-        switch (menu)
+        while (true)
         {
-            case 1:
-                System.out.println("Insert your text");
-                String text = scanner.next();
-                System.out.println("Insert shift: ");
-                int shift = scanner.nextInt();
-                System.out.println(coding.coding(text, shift));
+            System.out.println("1. Coding text, 2. Decodint text ");
+            int menu = scanner.nextInt();
+            switch (menu)
+            {
+                case 1:
+                    System.out.println("Insert your text");
+                    String text = scanner.next();
+                    System.out.println("Insert shift: ");
+                    int shift = scanner.nextInt();
+                    System.out.println(coding.coding(text, shift));
 
-                break;
+                    SaveToFile saveToFile = new SaveToFile(coding.coding(text, shift));
+                    saveToFile.saveToFile();
+                    break;
 
-            case 2:
-                break;
+                case 2:
+                    System.out.println("Insert your text");
+                    text = scanner.next();
+                    System.out.println("Insert shift: ");
+                    shift = scanner.nextInt();
+                    System.out.println(coding.deCoding(text, shift));
+                    break;
+            }
         }
+    }
+    public static void main(String[] args)
+    {
+       start();
+
 
 
     }
---
+++ src/file/SaveToFile.java
@@ -0,0 +1,30 @@
+package file;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class SaveToFile
+{
+    private String text;
+
+    public SaveToFile(String text)
+    {
+        this.text = text;
+    }
+
+    public void saveToFile()
+    {
+        try (FileOutputStream f = new FileOutputStream(new File("/home/dominik/Pobrane/secret.txt")))
+        {
+            ObjectOutputStream o = new ObjectOutputStream(f);
+            o.writeObject(text);
+            o.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
+    }
+}
