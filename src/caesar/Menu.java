72c0e88fdab3330b4b31c12f5a52af472a9cc21c
dominik <dominwro@gmail.com>
2019-06-03 11:39:30 +0200

    Initial commit.

--- src/caesar/Menu.java
+++
@@ -1,9 +0,0 @@
-package caesar;
-
-public class Menu
-{
-    public static void main(String[] args)
-    {
-
-    }
-}
---
+++ src/caesar/main/Coding.java
@@ -0,0 +1,38 @@
+package caesar.main;
+
+
+public class Coding
+{
+    public String coding (String text, int shift)
+    {
+        char[] chars = text.toCharArray();
+
+        for (int i = 0; i < chars.length; i++)
+        {
+            if (chars[i] + shift > 90)
+            {
+                chars[i] = (char) (64 + (shift - (90 - chars[i])));
+            }
+            else
+            {
+                chars[i] += shift;
+            }
+        }
+        String product = new String(chars);
+
+        return product;
+    }
+
+    public String deCoding (String text, int shift)
+    {
+        char[] chars = text.toCharArray();
+
+        for (int i = 0; i < chars.length; i++)
+        {
+            chars[i] -= shift;
+        }
+        String product = new String(chars);
+
+        return product;
+    }
+}
